40a82c40be721718356b856c3c206835f7373eb8
¨Mario <mario.vizcainos@gmail.com>
2016-03-22 21:57:13 -0500

    method post to create new post/threads

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,24 @@
 class PostController < ApplicationController
+  before_action :set_board, only: [:all, :create]
   def all
-    board = Board.where(name: params[:name])[0]
-    post = Post.where(board_id: board.id)
+    post = Post.where(board_id: @board.id)
     render json: post , status: 200
+  end
+  def create
+    post = Post.new(create_params)
+    post.board_id = @board.id
+    if post.save
+      render json: post, status: 200
+    else
+      render json: "error".to_json, status: 200
+    end
+  end
+
+  private
+  def set_board
+    @board = Board.where(name: params[:name])[0]
+  end
+  def create_params
+    params.require(:post).permit(:title, :user_ip)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'boards', to: 'boards#all'
   get '/:name', to: 'post#all'
+  post '/:name', to: 'post#create'
 end
