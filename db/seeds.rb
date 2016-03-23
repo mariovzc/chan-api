6dcf9b89da5ba62717c5977b5aedea86275b2920
¨Mario <mario.vizcainos@gmail.com>
2016-03-22 21:02:05 -0500

    added seed of boards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Board.where(name: 'b').first_or_create
+Board.where(name: 'x').first_or_create
+Board.where(name: 'pol').first_or_create
