6243a313e2c9256bf201aee372477b196b849c8b
¨Mario <mario.vizcainos@gmail.com>
2016-03-28 22:06:20 -0500

    model validation of post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :board
   has_many :comments, dependent: :destroy
+
+  #Validations
+  validates :title, presence: true
+  validates :user_ip, presence: true
 end
