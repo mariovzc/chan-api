65239d99617f96381d5b096f52adf9955537e9a8
¨Mario <mario.vizcainos@gmail.com>
2016-03-28 22:03:19 -0500

    add: new methods to create, list and show comments. Added new routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,36 @@
 class CommentsController < ApplicationController
+  before_action :current_board, only: [:list]
+  before_action :current_post, only: [:list, :create]
+
+  #  get '/:board/:id', to: 'comment#list'
+  def list
+    comments = Comment.where(post: @post)
+    render json: comments, status: 200
+  end
+  #  post '/:board/:post', to: 'comments#create'
+  def create
+    comment = Comment.new(create_params)
+    comment.post = @post
+    if comment.save
+      render json: comment, status: 200
+    else
+      render json: "error".to_json, status: 403
+    end
+
+  end
+  #get '/:board/:post/:id', to: 'comments#show'
+  def show
+    comment = Comment.find(params[:id])
+    render json: comment, status: 200
+  end
+  private
+  def current_board
+    @board = Board.where(name: params[:board])[0]
+  end
+  def current_post
+    @post = Post.find(params[:post])
+  end
+  def create_params
+    params.require(:comment).permit(:body, :user_ip)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  #list all the boards - listar todas las boards
   get 'boards', to: 'boards#all'
+  #list all the post - listar todos los post -> params board name
   get '/:name', to: 'post#all'
+  #list all the comments - listar todos los comentarios  -> params board name, and post id
+  get '/:board/:post', to: 'comments#list'
+  #get the current comment - obtener el comentario -> params board name, post id and comment id
+  get '/:board/:post/:id', to: 'comments#show'
+
+  #create a new post - crear un nuevo post -> params url: board name, body : title, user_ip
   post '/:name', to: 'post#create'
+  #create a new comment - crear un nuevo comentario -> params
+  post '/:board/:post', to: 'comments#create'
 end
