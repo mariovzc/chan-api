7c31cbc770e11f0b5db3c9db32fc882adbf653a9
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 12:17:29 -0500

    added model validation for the lenght of the characters in post title, and description

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   #Validations
-  validates :title, :body, presence: true
+  validates :title, presence: true
+  validates_length_of :title, :minimum => 10, :maximum => 60
+
+  validates_length_of :body, :minimum => 10, :maximum => 100, :allow_blank => true
   validates :user_ip, presence: true
 end
