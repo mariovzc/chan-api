25beb77b36f4bc2b83bff9653eab66a2cef64b9c
¨Mario <mario.vizcainos@gmail.com>
2016-04-02 12:41:51 -0500

    added post serializer

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,10 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body, :user_ip, :created, :posts
+  def created
+    object.created_at.to_formatted_s(:db)
+  end
+  def posts
+    p = Post.where(board_id: object.id)
+    p.count
+  end
+end
